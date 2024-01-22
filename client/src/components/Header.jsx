65e6469628d3e83527a4f2a54ff72f6cdc8e28a1
yashdatir1999 <yashdatir1999@gmail.com>
2024-01-22 20:33:47 +0530

    create the header section

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import {Navbar, TextInput , Button} from 'flowbite-react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
 
 const Header = () => {
+    const path = useLocation().pathname
   return (
         <Navbar className='border-b-2'>
             <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -28,15 +29,16 @@ const Header = () => {
                     <Link to="/sihn-in">
                         <Button gradientDuoTone='purpleToBlue'>Sign In</Button>
                     </Link>
+                    <Navbar.Toggle />
                 </div>
                     <Navbar.Collapse>
-                        <Navbar.Link>
+                        <Navbar.Link active={path === "/"} as={'div'}>
                             <Link to="/">Home</Link>
                         </Navbar.Link>
-                        <Navbar.Link>
+                        <Navbar.Link active={path === "/about"} as={'div'}>
                             <Link to="/about">About</Link>
                         </Navbar.Link>
-                        <Navbar.Link>
+                        <Navbar.Link active={path === "/projects"} as={'div'}>
                             <Link to="/projects">Projects</Link>
                         </Navbar.Link>
                     </Navbar.Collapse>
@@ -45,3 +47,4 @@ const Header = () => {
 }
 
 export default Header
+
