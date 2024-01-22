a5e17ea23ffe433ac8cc2375cf9a21f26220c208
yashdatir1999 <yashdatir1999@gmail.com>
2024-01-22 17:28:53 +0530

    nav complete

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import {Navbar, TextInput} from 'flowbite-react'
+import {Navbar, TextInput , Button} from 'flowbite-react'
 import { Link } from 'react-router-dom'
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
 
 const Header = () => {
   return (
@@ -11,9 +13,33 @@ const Header = () => {
             <form>
                 <TextInput 
                     type='text'
-                    placeholder='Search'
+                    placeholder='Search...'
+                    rightIcon={AiOutlineSearch }
+                    className='hidden lg:inline'
                 />
             </form>
+                <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+                    <AiOutlineSearch/>
+                </Button>
+                <div className='flex gap-2 md:order-2'>
+                    <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
+                    <FaMoon /> 
+                    </Button>
+                    <Link to="/sihn-in">
+                        <Button gradientDuoTone='purpleToBlue'>Sign In</Button>
+                    </Link>
+                </div>
+                    <Navbar.Collapse>
+                        <Navbar.Link>
+                            <Link to="/">Home</Link>
+                        </Navbar.Link>
+                        <Navbar.Link>
+                            <Link to="/about">About</Link>
+                        </Navbar.Link>
+                        <Navbar.Link>
+                            <Link to="/projects">Projects</Link>
+                        </Navbar.Link>
+                    </Navbar.Collapse>
         </Navbar>
     )
 }
