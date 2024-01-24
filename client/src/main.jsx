7d31c9ac9710e170e77b79b68c29242c9cd99c50
yashdatir1999 <yashdatir1999@gmail.com>
2024-01-24 15:56:25 +0530

    add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import {store} from './redux/store.js'
+import {store , persistor} from './redux/store.js'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <PersistGate persistor={persistor}>
   <Provider store={store}>
   <BrowserRouter>
   <App />
   </BrowserRouter>
   </Provider>
+  </PersistGate>
 )
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -88,3 +88,4 @@ const SignIn = () => {
 }
 
 export default SignIn
+
